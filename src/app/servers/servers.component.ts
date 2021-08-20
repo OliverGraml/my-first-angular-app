a462a915014611445ddd639025848005243b443a
Oliver Graml <oliver.graml@adesso.de>
2021-08-20 11:47:52 +0200

    Did the exercises from section 3 of the course

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -11,6 +11,7 @@ export class ServersComponent implements OnInit {
   serverCreationStatus='No server was created!'
   serverName = 'Testserver';
   serverCreated = false;
+  servers = ['Testserver', 'Testserver 2'];
   
   constructor() { 
     setTimeout(() => {this.allowNewServer=true}, 2000);
@@ -20,6 +21,7 @@ export class ServersComponent implements OnInit {
   }
     onCreateServer() {
       this.serverCreated=true;
+      this.servers.push(this.serverName);
       this.serverCreationStatus='Server was created! Name is ' + this.serverName;
   }
     onUpdateServerName(event: Event) {
